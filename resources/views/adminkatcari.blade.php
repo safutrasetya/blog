686c6300a22a0660ca4b59ab9eac84f784bc645a
immanuel1210 <immanuelsimarsoit@gmail.com>
2021-12-08 00:30:41 +0700

    8/12 adminkatcari

--- resources/views/adminkatcari.blade.php
+++ resources/views/adminkatcari.blade.php
@@ -50,7 +50,6 @@
                 <th>Nama</th>
                 <th>Gambar</th>
                 <th>Info</th>
-                <th>Jumlah Artikel</th>
                 <th>Action</th>
               </tr>
             </thead>
@@ -66,12 +65,11 @@
                   </div>
 
                 </td>
-                <td></td>
                 <td>
-                    <input type="text" value="" hidden>
-                    <button class="btn btn-success mb-2"><img src="img/edit-icon.png" style="height:20px; width:26px;"> Edit</button>
+                  <input type="text" value="" hidden>
+                    <a href="editkategori/{{$data->id_kat}}"><button class="btn btn-success mb-2"><img src="img/edit-icon.png" style="height:20px; width:26px;"> Edit</button></a>
                     <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalhapus" data-bs-whatever="{{$data->id_kat}}"
-                      namakat="{{$data->nama_kat}}"><img src="img/trash-can.png" style="height:20px; width:15px;"> Hapus</button>
+                    namakat="{{$data->nama_kat}}"><img src="img/trash-can.png" style="height:20px; width:15px;"> Hapus</button>
                       <!--Modal-->
                       <div class="modal fade" id="modalhapus" tabindex="-1" aria-hidden="true">
                         <div class="modal-dialog">
